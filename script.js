a04753f0d8eee517b182d24e64cd629d7c14a30d
r2dynamic <r2dynamicengineering@gmail.com>
2025-02-19 19:11:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -22,32 +22,313 @@
  
   // --- Mappings & Data ---
   const cityFullNames = {
-    "ALP": "Alpine",
-    "ALT": "Alta",
-    "ALM": "Altamont",
-    "ALN": "Alton",
-    "AMG": "Amalga",
-    "AFK": "American Fork",
-    "ANB": "Annabella",
-    "ANT": "Antimony",
-    "APV": "Apple Valley",
-    "ARA": "Aurora",
-    "BAL": "Ballard",
-    "BRV": "Bear River",
-    "BVR": "Beaver",
-    "BRL": "Beryl",
-    "BKL": "Bicknell",
-    "BGW": "Big Water",
-    "BLA": "Blanding",
-    "BMT": "Bloomington",
-    "BLF": "Bluffdale",
-    "BLD": "Boulder",
-    "BTF": "Bountiful",
-    "BTN": "Brighton",
-    "BHD": "Brian Head",
-    "BRC": "Brigham City",
-    "BCC": "Bryce Canyon City"
-    // ... additional mappings as needed
+   "ALP": "Alpine",
+      "ALT": "Alta",
+      "ALM": "Altamont",
+      "ALN": "Alton",
+      "AMG": "Amalga",
+      "AFK": "American Fork",
+      "ANB": "Annabella",
+      "ANT": "Antimony",
+      "APV": "Apple Valley",
+      "ARA": "Aurora",
+      "BAL": "Ballard",
+      "BRV": "Bear River",
+      "BVR": "Beaver",
+      "BRL": "Beryl",
+      "BKL": "Bicknell",
+      "BGW": "Big Water",
+      "BLA": "Blanding",
+      "BMT": "Bloomington",
+      "BLF": "Bluffdale",
+      "BLD": "Boulder",
+      "BTF": "Bountiful",
+      "BTN": "Brighton",
+      "BHD": "Brian Head",
+      "BRC": "Brigham City",
+      "BCC": "Bryce Canyon City",
+      "CNV": "Cannonville",
+      "CNR": "Canyon Rim",
+      "CDL": "Castle Dale",
+      "CVY": "Castle Valley",
+      "CDC": "GA",
+      "CDF": "Cedar Fort",
+      "CDH": "Cedar Hills",
+      "CNF": "Centerfield",
+      "CVL": "Centerville",
+      "CEV": "Central Valley",
+      "CHR": "Charleston",
+      "CRV": "Circleville",
+      "CLK": "Clarkston",
+      "CLW": "Clawson",
+      "CFD": "Clearfield",
+      "CVD": "Cleveland",
+      "CTN": "Clinton",
+      "CLV": "Coalville",
+      "CRN": "Corinne",
+      "CNS": "Cornish",
+      "CWH": "Cottonwood Heights",
+      "CWW": "Cottonwood West",
+      "DAN": "Daniel",
+      "DLT": "Delta",
+      "DEW": "Deweyville",
+      "DPR": "Draper",
+      "DCH": "Duchesne",
+      "DUG": "Dugway",
+      "EAG": "Eagle Mountain",
+      "ECB": "East Carbon",
+      "EMC": "East Millcreek",
+      "ELK": "Elk Ridge",
+      "EMO": "Elmo",
+      "ELS": "Elsinore",
+      "ELW": "Elwood",
+      "EMR": "Emery",
+      "ENO": "Enoch",
+      "ENT": "Enterprise",
+      "EPH": "Ephraim",
+      "ERD": "Erda",
+      "ESC": "Escalante",
+      "EUR": "Eureka",
+      "FRF": "Fairfield",
+      "FRV": "Fairview",
+      "FRM": "Farmington",
+      "FRW": "Farr West",
+      "FAY": "Fayette",
+      "FRN": "Ferron",
+      "FLD": "Fielding",
+      "FIL": "Fillmore",
+      "FTD": "Fort Duchesne",
+      "FOU": "Fountain Green",
+      "FRA": "Francis",
+      "FRU": "Fruit Heights",
+      "GRC": "Garden City",
+      "GAR": "Garland",
+      "GEN": "Genola",
+      "GDL": "Glendale",
+      "GWD": "Glenwood",
+      "GOS": "Goshen",
+      "GNT": "Granite",
+      "GVL": "Grantsville",
+      "GRR": "Green River",
+      "GUN": "Gunnison",
+      "HKV": "Hanksville",
+      "HRV": "Harrisville",
+      "HAT": "Hatch",
+      "HBR": "Heber City",
+      "HLP": "Helper",
+      "HFR": "Henefer",
+      "HNV": "Henrieville",
+      "HRR": "Herriman",
+      "HIA": "Hiawatha",
+      "HLD": "Highland",
+      "HDL": "Hildale",
+      "HKY": "Hinckley",
+      "HDN": "Holden",
+      "HDY": "Holladay",
+      "HYV": "Honeyville",
+      "HPR": "Hooper",
+      "HWL": "Howell",
+      "HTN": "Huntington",
+      "HTV": "Huntsville",
+      "HRC": "Hurricane",
+      "HYD": "Hyde Park",
+      "HYR": "Hyrum",
+      "IVN": "Ivins",
+      "JSP": "Joseph",
+      "JCT": "Junction",
+      "KMS": "Kamas",
+      "KNB": "Kanab",
+      "KRV": "Kanarraville",
+      "KSH": "Kanosh",
+      "KAY": "Kaysville",
+      "KRN": "Kearns",
+      "KTY": "Keetley",
+      "KNG": "Kingston",
+      "KOO": "Koosharem",
+      "LVR": "La Verkin",
+      "LKP": "Lake Point",
+      "LKT": "Laketown",
+      "LTN": "Layton",
+      "LMT": "Leamington",
+      "LED": "Leeds",
+      "LHI": "Lehi",
+      "LVN": "Levan",
+      "LEW": "Lewiston",
+      "LDN": "Lindon",
+      "LOA": "Loa",
+      "LGN": "Logan",
+      "LUN": "Lund",
+      "LYM": "Lyman",
+      "LDL": "Lynndyl",
+      "MAE": "Maeser",
+      "MAG": "Magna",
+      "MNL": "Manila",
+      "MTI": "Manti",
+      "MTU": "Mantua",
+      "MPL": "Mapleton",
+      "MSV": "Marriott-Slaterville",
+      "MRV": "Marysvale",
+      "MAY": "Mayfield",
+      "MDW": "Meadow",
+      "MDN": "Mendon",
+      "MXH": "Mexican Hat",
+      "MDV": "Midvale",
+      "MWY": "Midway",
+      "MFD": "Milford",
+      "MCK": "Millcreek",
+      "MLV": "Millville",
+      "MAB": "Moab",
+      "MNA": "Mona",
+      "MRO": "Monroe",
+      "MZC": "Montezuma Creek",
+      "MNC": "Monticello",
+      "MGN": "Morgan",
+      "MRN": "Moroni",
+      "MCR": "Mt. Carmel",
+      "MCJ": "Mt. Carmel Junction",
+      "MTO": "Mt. Olympus",
+      "MTP": "Mt. Pleasant",
+      "MUR": "Murray",
+      "MYT": "Myton",
+      "NPL": "Naples",
+      "NEO": "Neola",
+      "NPH": "Nephi",
+      "NCL": "New Castle",
+      "NHR": "New Harmony",
+      "NTN": "Newton",
+      "NIB": "Nibley",
+      "NLG": "North Logan",
+      "NOG": "North Ogden",
+      "NSL": "North Salt Lake",
+      "OAC": "Oak City",
+      "OKY": "Oakley",
+      "OGD": "Ogden",
+      "OPH": "Ophir",
+      "OQR": "Oquirrh",
+      "OGV": "Orangeville",
+      "ODV": "Orderville",
+      "ORM": "Orem",
+      "PNG": "Panguitch",
+      "PDS": "Paradise",
+      "PGH": "Paragonah",
+      "PKC": "Park City",
+      "PWN": "Parowan",
+      "PSN": "Payson",
+      "PRY": "Perry",
+      "PLC": "Plain City",
+      "PLG": "Pleasant Grove",
+      "PLV": "Pleasant View",
+      "PLY": "Plymouth",
+      "PTG": "Portage",
+      "PRC": "Price",
+      "PVD": "Providence",
+      "PVO": "Provo",
+      "RDT": "Randlett",
+      "RAN": "Randolph",
+      "RED": "Redmond",
+      "RFD": "Richfield",
+      "RMD": "Richmond",
+      "RHT": "River Heights",
+      "RDL": "Riverdale",
+      "RVT": "Riverton",
+      "RKV": "Rockville",
+      "RKR": "Rocky Ridge",
+      "RSV": "Roosevelt",
+      "ROY": "Roy",
+      "RVY": "Rush Valley",
+      "SLM": "Salem",
+      "SLA": "Salina",
+      "SLC": "Salt Lake City",
+      "SND": "Sandy",
+      "SCL": "Santa Clara",
+      "STQ": "Santaquin",
+      "SSP": "Saratoga Springs",
+      "SCP": "Scipio",
+      "SCO": "Scofield",
+      "SIG": "Sigurd",
+      "SMF": "Smithfield",
+      "SNO": "Snowville",
+      "SOL": "Soldier Summit",
+      "SJO": "South Jordan",
+      "SOG": "South Ogden",
+      "SSL": "South Salt Lake",
+      "SWE": "South Weber",
+      "SPF": "Spanish Fork",
+      "SPC": "Spring City",
+      "SDL": "Springdale",
+      "SPV": "Springville",
+      "STG": "St. George",
+      "STP": "Stansbury Park",
+      "STE": "Sterling",
+      "STO": "Stockton",
+      "SMT": "Summit",
+      "SNY": "Sunnyside",
+      "SUN": "Sunset",
+      "SYR": "Syracuse",
+      "TAB": "Tabiona",
+      "TAY": "Taylorsville",
+      "THI": "Thistle",
+      "TLE": "Tooele",
+      "TOQ": "Toquerville",
+      "TOR": "Torrey",
+      "TRE": "Tremonton",
+      "TNT": "Trenton",
+      "TRO": "Tropic",
+      "UIN": "Uintah",
+      "UNI": "Union",
+      "VAL": "Val Verda",
+      "VNL": "Vernal",
+      "VRN": "Vernon",
+      "VEY": "Veyo",
+      "VIN": "Vineyard",
+      "VRG": "Virgin",
+      "WAL": "Wales",
+      "WBG": "Wallsburg",
+      "WAS": "Washington",
+      "WTE": "Washington Terrace",
+      "WTN": "Wellington",
+      "WVL": "Wellsville",
+      "WEN": "Wendover",
+      "WBN": "West Bountiful",
+      "WHV": "West Haven",
+      "WJD": "West Jordan",
+      "WPT": "West Point",
+      "WVC": "West Valley City",
+      "WHC": "White City",
+      "WRK": "Whiterocks",
+      "WIL": "Willard",
+      "WLH": "Woodland Hills",
+      "WDF": "Woodruff",
+      "WXS": "Woods Cross",
+      "UT": "Utah",
+      "SL": "Salt Lake",
+      "DU": "Duchesne",
+      "KN": "Kane",
+      "CA": "Cache",
+      "CC": "Carbon",
+      "SE": "Sevier",
+      "UN": "Uintah",
+      "BE": "Box Elder",
+      "BV": "Beaver",
+      "RN": "Iron",
+      "WE": "Wayne",
+      "SJ": "San Juan",
+      "WN": "Washington",
+      "DA": "Davis",
+      "PT": "Piute",
+      "EM": "Emery",
+      "GR": "Grand",
+      "WA": "Wasatch",
+      "MD": "Millard",
+      "JU": "Juab",
+      "DG": "Daggett",
+      "GA": "Garfield",
+      "MN": "Morgan",
+      "RI": "Rich",
+      "SP": "Sanpete",
+      "SU": "Summitt",
+      "TE": "Tooele",
+      "WB": "Weber"
   };
  
   const regionCities = {
@@ -458,28 +739,28 @@ document.addEventListener('DOMContentLoaded', () => {
   // For each dropdown toggle, add mouseenter/mouseleave to delay auto-close when interacting
   const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
   dropdownToggles.forEach(toggle => {
-      let closeTimer;
-      const dropdownMenu = toggle.nextElementSibling;
-      toggle.addEventListener('shown.bs.dropdown', function () {
-          // Hide any other open dropdowns immediately
-          dropdownToggles.forEach(otherToggle => {
-            if (otherToggle !== toggle) {
-              const instance = bootstrap.Dropdown.getInstance(otherToggle);
-              if (instance) instance.hide();
-            }
-          });
+    let closeTimer;
+    const dropdownMenu = toggle.nextElementSibling;
+    toggle.addEventListener('shown.bs.dropdown', function () {
+      // Hide any other open dropdowns immediately
+      dropdownToggles.forEach(otherToggle => {
+        if (otherToggle !== toggle) {
+          const instance = bootstrap.Dropdown.getInstance(otherToggle);
+          if (instance) instance.hide();
+        }
       });
-      if (dropdownMenu) {
-          dropdownMenu.addEventListener('mouseenter', () => {
-              clearTimeout(closeTimer);
-          });
-          dropdownMenu.addEventListener('mouseleave', () => {
-              closeTimer = setTimeout(() => {
-                  const instance = bootstrap.Dropdown.getInstance(toggle);
-                  if (instance) instance.hide();
-              }, 5000);
-          });
-      }
+    });
+    if (dropdownMenu) {
+      dropdownMenu.addEventListener('mouseenter', () => {
+        clearTimeout(closeTimer);
+      });
+      dropdownMenu.addEventListener('mouseleave', () => {
+        closeTimer = setTimeout(() => {
+          const instance = bootstrap.Dropdown.getInstance(toggle);
+          if (instance) instance.hide();
+        }, 5000);
+      });
+    }
   });
   
   // Close dropdowns if clicking outside
@@ -554,4 +835,38 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+  
+  // --- Draggable Modal ---
+  const imageModal = document.getElementById("imageModal");
+  if (imageModal) {
+    const modalDialog = imageModal.querySelector(".draggable-modal");
+    const modalHeader = imageModal.querySelector(".modal-header");
+    let isDragging = false, offsetX = 0, offsetY = 0;
+    
+    modalHeader.addEventListener("mousedown", function(e) {
+      isDragging = true;
+      const rect = modalDialog.getBoundingClientRect();
+      offsetX = e.clientX - rect.left;
+      offsetY = e.clientY - rect.top;
+      modalDialog.style.transition = "none"; // Disable transition during drag
+    });
+    
+    document.addEventListener("mousemove", function(e) {
+      if (isDragging) {
+        modalDialog.style.left = (e.clientX - offsetX) + "px";
+        modalDialog.style.top = (e.clientY - offsetY) + "px";
+      }
+    });
+    
+    document.addEventListener("mouseup", function(e) {
+      isDragging = false;
+      modalDialog.style.transition = ""; // Restore transition
+    });
+    
+    // Reset modal position when shown
+    imageModal.addEventListener("shown.bs.modal", function() {
+      modalDialog.style.left = "";
+      modalDialog.style.top = "";
+    });
+  }
 });
