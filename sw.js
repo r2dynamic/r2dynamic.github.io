8d305bdbffa7c5148ef346ee908cd8c28ce4dac3
r2dynamic <r2dynamicengineering@gmail.com>
2025-03-05 18:01:20 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,8 @@ const FILES_TO_CACHE = [
   '/cameras.json',
   '/manifest.json',
   '/camIcon1.png',
+  '/Icongridbackground.png',
+  '/cameraData.js',
   '/offline.html',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
