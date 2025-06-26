7d8264c9d311532294fc9a5d6f527b115a7ad188
r2dynamic <r2dynamicengineering@gmail.com>
2025-06-26 13:08:54 -0600

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker
 
-const CACHE_VERSION = 'v34';
+const CACHE_VERSION = 'v35';
 const PRECACHE_NAME = `wpa-precache-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `wpa-runtime-${CACHE_VERSION}`;
 
